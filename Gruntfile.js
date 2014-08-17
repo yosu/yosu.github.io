7cd9b903edd7cbf226a3b57921897ac4b6b51673
yosu <woodstock830@gmail.com>
2014-08-17 15:40:14 +0900

    Setup grunt for editting with livereload

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+// Gruntfile.js
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        shell: {
+            jekyllServe: {
+                command: 'jekyll serve --watch'
+            }
+        },
+        watch: {
+            files: [
+                '_site/*'
+            ],
+            options: {
+                livereload: true
+            }
+        },
+        concurrent: {
+            target: {
+                tasks: ['shell:jekyllServe', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.registerTask('default', ['concurrent:target']);
+};
